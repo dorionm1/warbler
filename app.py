2040ccbc0e131399eddcb8fcc45403d460d96541
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-11-29 20:41:06 -0500

    added code to /logout route to bring user back to login page and remove them from session

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -114,7 +114,10 @@ def logout():
     """Handle logout of user."""
 
     # IMPLEMENT THIS
+    do_logout()
+    flash("Logout Successful!")
 
+    return redirect('/login')
 
 ##############################################################################
 # General user routes:
